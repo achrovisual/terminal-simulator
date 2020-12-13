088686239f2a1c5d73da937e9ac37e38a4299f1a
eugeniopastoral <stonenobrein@gmail.com>
2020-12-13 17:15:15 +0800

    Added input handling and shell prompts

--- terminal.c
+++ terminal.c
@@ -3,16 +3,56 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <time.h>
+#include <string.h>
 
-int main() {
-  char cwd[PATH_MAX];
-  time_t rawtime;
-  struct tm * timeinfo;
+int get_input(char *str) {
+  char buf[120];
 
-  char string_input[120] = "Hello World";
+  scanf("%[^\n]%*c", buf);
+  // printf("%s", buf);
+  if (strlen(buf) != 0) {
+    strcpy(str, buf);
+    return 0;
+  } else {
+    return 1;
+  }
+}
 
-  int arg_id = 4;
+int parse_input(char *string_input, char *string_command, char *string_echo, int *arg_id) {
+  char delimiter[] = " ";
+  char *remainder, *context, *temp;
 
+  int length = strlen(string_input);
+  char *input_copy = (char*) calloc(length + 1, sizeof(char));
+  strncpy(input_copy, string_input, length);
+
+  temp = strtok_r (input_copy, delimiter, &context);
+  string_echo = context;
+
+  strcpy(string_command, temp);
+
+  free(input_copy);
+
+  if (strcmp(string_command, "pwd") == 0) {
+    *arg_id = 1;
+  }
+  else if (strcmp(string_command, "time") == 0) {
+    *arg_id = 2;
+  }
+  else if (strcmp(string_command, "clear") == 0) {
+    *arg_id = 3;
+  }
+  else if (strcmp(string_command, "say") == 0) {
+    *arg_id = 4;
+  }
+  else if (strcmp(string_command, "exit") == 0) {
+    *arg_id = 7;
+  }
+
+  return 1;
+}
+
+void command_handler(int arg_id, char *string_input) {
   /*
   ARGS ID values
 
@@ -22,24 +62,53 @@ int main() {
   4 = say // echo string input
   */
 
+  char cwd[PATH_MAX];
+
   if (arg_id == 1) {
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
       printf("Current working dir: %s\n", cwd);
     } else {
       perror("getcwd() error");
-      return 1;
     }
   }
   else if (arg_id == 2) {
+    time_t rawtime;
+    struct tm * timeinfo;
+
     time ( &rawtime );
     timeinfo = localtime ( &rawtime );
     printf ( "Current local time and date: %s", asctime (timeinfo) );
   }
   else if (arg_id == 3) {
-    system("cls");
+    system("clear");
   }
   else if (arg_id == 4) {
     printf("%s\n", string_input);
   }
+}
+
+int main() {
+  char string_input[120] = "Hello World";
+  char string_command[120] = "Hello World";
+  char string_echo[120] = "Hello World";
+
+  int execution_flag = 0;
+  int arg_id = 0;
+
+  while (1) {
+    // take input
+    printf("MyOS> ");
+    if (get_input(string_input))
+      continue;
+    // break;
+
+    execution_flag = parse_input(string_input, string_command, string_echo, &arg_id);
+    command_handler(arg_id, string_echo);
+
+    if (arg_id == 7) {
+      return 0;
+    }
+  }
+
   return 0;
 }
